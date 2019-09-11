0458cf7379e1e5077f0d8a520cf6de9c2bd34c0c
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-09-11 13:28:39 -0500

    Intial commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function NavBar(){
+  let navBar =
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="index.html">
+      <img className="img-fluid" src="images/Logo.PNG" alt="Logo" width="200" heigth="50"></img>
+    </a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Disabled</a>
+        </li>
+      </ul>
     </div>
-  );
+  </nav>;
+  return (navBar);
 }
 
-export default App;
+/* function NavFooter(){
+  let footer =
+  <div className="row">
+    <div className="col">
+      <span id="face">Facebook</span>
+    </div>
+    <div className="col">
+      <span id="insta">Instagram</span>
+    </div>
+    <div className="col">
+      <span id="twit">Twitter</span>
+    </div>
+  </div>;
+  return footer;
+} */
+
+export default NavBar;
+
+/* ReactDOM.render(
+  <NavFooter />,
+  document.getElementById("footer")
+) */
