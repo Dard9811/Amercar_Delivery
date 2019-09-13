d1012a88252c186365646b934e117c58013991d7
Daniel Ramirez <da.ramirez14@uniandes.edu.co>
2019-09-13 03:19:21 -0500

    Connecting with mongodb

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,30 @@
 var express = require("express");
 var router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017";
+
+const client = new MongoClient(url);
 
 /* GET home page. */
 router.get("/data", function(req, res, next) {
-  res.render("index", { title: "Express" });
+
+  client.connect(
+    () =>{
+      const db = client.db("amercar");
+      const colProducto = db.collection("producto");
+
+      colProducto.find({}).limit(10).toArray(
+      (err, productos) => {
+        if (err) throw err;
+
+        console.log("# Productos: ", productos.length);
+      }
+      );
+
+      client.close();
+    }
+  );
 });
 
 module.exports = router;
